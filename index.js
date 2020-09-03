e2c8fb565f6f07ff15b09db32fe2114b3288ec20
Andreas Zoellner <andreaszoellner@gmail.com>
2020-09-03 15:48:54 +0200

    simplify demo

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const {TestClass} = require('./testClass');
+const AWS = require('aws-sdk');
 
 exports.handler = async function handleEvent(event) {
   console.log(event);
--- testClass.js
+++
@@ -1,9 +0,0 @@
-const AWS = require('aws-sdk')
-
-class TestClass {
-  constructor() {
-    this.client = new AWS.SQS();
-  }
-}
-
-module.exports = {TestClass};
